db0e56eb9f33abba80fbf2732a8d9cfb3ff990c1
Armin Sabouri <armin@reggora.com>
2018-03-09 21:52:16 -0500

    Backend feature for rate my professor API

--- back-end-platform/app.js
+++ back-end-platform/app.js
@@ -6,6 +6,10 @@
 var express = require('express')
   , routes = require('./routes');
 
+var request = require('request');
+var cheerio = require('cheerio');
+
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -34,6 +38,72 @@ app.get('/', function(req, res){
   res.end();
 });
 
-app.listen(3000, function(){
+
+
+app.get('/rmp/:uniName', function(req, res){
+
+
+  let uniName = req.params.uniName;
+  let url = 'http://www.ratemyprofessors.com/search.jsp?query='+ uniName;
+  let sid;
+  
+   
+  request(url, function(error, response, html){
+      if(!error){
+          var $ = cheerio.load(html);
+
+      let sid = $('div.listings-wrap ul.listings li.SCHOOL a').attr("href");
+
+      if(sid == undefined || sid == null){
+        res.send("Incorrect Response");
+        return;
+      }
+
+      sid = sid.split("?")[1];
+      console.log("the sid");
+      console.log(sid);
+
+      //request to get info
+          //URL EXAMPLE http://www.ratemyprofessors.com/campusRatings.jsp?sid=1280
+
+          let ratingURL =  "http://www.ratemyprofessors.com/campusRatings.jsp?" + sid;
+          console.log("the rating url and sid");
+          console.log(ratingURL, sid);
+          request(ratingURL, function(error, response, html){
+            if(!error){
+                var $ = cheerio.load(html);
+
+            let ratings = {};
+            $(".school-averages div.rating").each(function(index){
+                if(index > 7)
+                  return;
+                let rating = $(this).children()[1];
+                let cat =  $(this).children()[2];
+                ratings[$(cat).text()] = $(rating).text();
+                //console.log($(rating).text(), $(cat).text());  
+
+            });
+            console.log(ratings);
+            res.json(ratings);
+            
+
+            }
+
+          });
+      
+      }
+
+    });//first request
+
+    
+
+
+
+//end of the rmp route
+    
+})
+
+
+app.listen(8080, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
