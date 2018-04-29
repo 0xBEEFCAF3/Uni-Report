3031ed2c6972986b26291cfcbb1afaec6e6cb57d
kimsoojee <36211186+kimsoojee@users.noreply.github.com>
2018-04-29 18:56:29 -0400

    Delete auth.js

--- back-end-platform/auth/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	"client_id": "493302779930-v247mbnb19vjp59lhqn7aj7d91vtavnd.apps.googleusercontent.com",
-	"client_pass": "frB_DqfNwLpu_jyVVuwDNL9v",
-	"JWT_secret": "559a6ab59d9a47c28584e8e42e98aa72"
-}
